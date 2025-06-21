0d6d414fd7919711c5c3f8779303eabe86038da6
CletoB <cletoantonello.berardi@raceup.it>
2025-06-21 10:15:51 +0200

    Fix(main): add panic handler

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@
 
 use embassy_executor::Spawner;
 use defmt_rtt as _;
+use panic_probe as _;
 use embassy_stm32::gpio::{Output, Level, Speed};
 
 
 #[embassy_executor::main]
-async fn main(spawner: Spawner) {
+async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     Output::new(p.PC6, Level::High, Speed::Low).set_high();
     Output::new(p.PC7, Level::High, Speed::Low).set_high();
